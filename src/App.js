be65a2c25a732a4ffa18188d67ae14ef211c3bf3
mtheusbrito <matheusbriodasilva@gmail.com>
2019-11-11 14:44:15 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import Test from "~/components/Test.js";
+import React, { Fragment } from "react";
 import Routes from "./routes";
 
 import { Provider } from "react-redux";
 
 import store from "./store";
+
+import GlobalStyle from "./styles/global";
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <Fragment>
+      <Routes />
+      <GlobalStyle />
+    </Fragment>
   </Provider>
 );
 export default App;
