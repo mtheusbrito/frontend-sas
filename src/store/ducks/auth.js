e12a3636b10d3f20257a0573d931a49694389db8
mtheusbrito <matheusbriodasilva@gmail.com>
2019-11-12 10:19:31 -0300

    duck configuraçao inicial

--- src/store/ducks/auth.js
+++ src/store/ducks/auth.js
@@ -1,1 +1,17 @@
- 
+import { createActions, createReducer } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+
+const { Types, Creators }  = createActions({
+
+});
+
+export const AuthTypes =  Types;
+ export default Creators; 
+
+ export const INITIAL_STATE = Immutable({
+
+ });
+
+ export const reducer   = createReducer(INITIAL_STATE, {
+     
+ });
