93e49a76d67ecffc220cfd40dc186e67e33382a6
mtheusbrito <matheusbriodasilva@gmail.com>
2019-12-11 17:47:45 -0300

    interceptor no axios, enviando o token

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import api from  '~/services/api';
 
 // import { Container } from './styles';
 class Main extends React.Component{
+    componentDidMount(){
+        api.get('/teste')
+    }
     render(){
         return (
             <div>
