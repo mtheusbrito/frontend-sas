8735c5e98597f8e7556411af5a3cc92f6c80f8f6
mtheusbrito <matheusbriodasilva@gmail.com>
2019-12-12 10:17:19 -0300

    alterando time ativo

--- src/store/ducks/teams.js
+++ src/store/ducks/teams.js
@@ -3,18 +3,29 @@ import { createReducer, createActions } from "reduxsauce";
 
 const { Types, Creators } = createActions({
   getTeamsRequest: null,
-  getTeamsSuccess: ["data"]
+  getTeamsSuccess: ["data"],
+  selectTeam: [''], 
+
 });
 
 export const TeamsTypes = Types;
 export default Creators;
 
 export const INITIAL_STATE = Immutable({
-  data: []
+  data: [], 
+  active: JSON.parse(localStorage.getItem('@Omni:team')) || null, 
 });
 
 //Reducers
 export const getSuccess = (state, { data }) => state.merge({ data });
+export const selectTeam = (state, {team}) => {
+  localStorage.setItem('@Omni:team', JSON.stringify(team));
+
+  return state.merge({ active: team});
+
+}
+
 export const reducer = createReducer(INITIAL_STATE, {
-  [Types.GET_TEAMS_SUCCESS]: getSuccess
+  [Types.GET_TEAMS_SUCCESS]: getSuccess,
+  [Types.SELECT_TEAM]: selectTeam
 });
